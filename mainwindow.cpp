c96b3edab95194c3cda61316f6e5c5d12b63b4f0
Oliverttt <501465967@qq.com>
2018-11-15 13:23:46 +0800

    init

---
+++ mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "rc4.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ;
+}
+
+void MainWindow::on_radioButton_2_clicked(bool checked)
+{
+    if(checked){
+        ;
+    }
+}
